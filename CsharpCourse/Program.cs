c55cf5f7f109229676ed36987a8f4b06afd7f174
Matheus-Bueno <38436505+Matheus-Bueno@users.noreply.github.com>
2025-09-30 16:12:55 -0300

    Initial commit
    cria solução CSharpCourse com projeto inicial

---
+++ CsharpCourse/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpCourse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
